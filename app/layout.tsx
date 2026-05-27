30373451157411893a83a88288af572b5c904d1f
Shahil-Ahamad-Per <shahilahamad04@gmail.com>
2026-05-27 07:31:19 +0545

    Update the Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
